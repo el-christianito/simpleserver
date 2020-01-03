efdf51a1fc5ef73bc4141176b076a58fd874b563
El Christianito <el_christianito@outlook.com>
2020-01-03 17:33:07 +0100

    add optional arg for serving other directories

--- wasm_server.py
+++ wasm_server.py
@@ -1,15 +1,26 @@
+import sys
 import http.server
 import socketserver
 
 PORT = 8000
 
-Handler = http.server.SimpleHTTPRequestHandler
-Handler.extensions_map.update({
+dir = None
+if len(sys.argv) > 1:
+    dir = sys.argv[1]
+    print('Serving directory ', sys.argv[1])
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=dir, **kwargs)
+
+handler = Handler
+'''http.server.SimpleHTTPRequestHandler(directory=dir)'''
+handler.extensions_map.update({
     '.wasm': 'application/wasm',
 })
 
 socketserver.TCPServer.allow_reuse_address = True
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with socketserver.TCPServer(("", PORT), handler) as httpd:
     httpd.allow_reuse_address = True
     print("serving at port", PORT)
     httpd.serve_forever()
